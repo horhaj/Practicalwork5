78f167fa31cf1438eaedeedd1153c6b1a940a6d2
horhaj <45919874+horhaj@users.noreply.github.com>
2021-02-07 16:23:53 +0100

    Add files via upload

--- code/src/test/java/fr/isen/java2/db/daos/TestRunner.java
+++ code/src/test/java/fr/isen/java2/db/daos/TestRunner.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class TestRunner {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("1 pour GenreTest, 2 pour FilmTest");
         int nbre = sc.nextInt();
+
+        // Test by choice 
         switch (nbre) {
             case 1:
                 GenreDaoTestCase test = new GenreDaoTestCase();
+
                 try {
                     test.initDatabase();
                     System.out.println("DataBase initiated");
+
                     test.shouldListGenres();
                     System.out.println("Genre listed");
+
                     test.shouldGetGenreByName();
                     System.out.println("Genre by Name gotten");
+
                     test.shouldNotGetUnknownGenre();
                     System.out.println("No Unknown genre gotten");
+
                     test.shouldAddGenre();
                     System.out.println("Genre added");
+
                 } catch (Exception e) {
                     System.out.println("erreur");
                     e.printStackTrace();
@@ -28,15 +37,20 @@ public class TestRunner {
                 break;
             case 2:
                 FilmDaoTestCase test2 = new FilmDaoTestCase();
+                
                 try {
                     test2.initDb();
                     System.out.println("DataBase initiated");
+
                     test2.shouldListFilms();
                     System.out.println("film listed");
+
                     test2.shouldListFilmsByGenre();
                     System.out.println("Film listed by Genre");
+
                     test2.shouldAddFilm();
                     System.out.println("Film added");
+
                 } catch (Exception e) {
                     System.out.println("erreur");
                     e.printStackTrace();
